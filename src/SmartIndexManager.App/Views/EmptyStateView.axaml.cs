2436f7eb39778429956612a44671360cb3c2645b
Rudi Bruchez <rudi@babaluga.com>
2026-07-21 12:50:22 +0200

    feat(app): reusable EmptyStateView

---
+++ src/SmartIndexManager.App/Views/EmptyStateView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SmartIndexManager.App.Views;
+
+public partial class EmptyStateView : UserControl
+{
+    public EmptyStateView() => AvaloniaXamlLoader.Load(this);
+}
